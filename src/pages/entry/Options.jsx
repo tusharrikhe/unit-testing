5e70c06a13a58139243f9d432846f6a55072f020
Tushar Rikhe <tusharrikhe95@gmail.com>
2022-08-03 15:17:54 +0530

    error handling in api

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,21 +3,26 @@ import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
-// import AlertBanner from '../common/AlertBanner';
+import AlertBanner from '../common/AlertBanner';
 // import { pricePerItem } from '../../constants';
 // import { useOrderDetails } from '../../contexts/OrderDetails';
 // import { formatCurrency } from '../../utilities';
 
 export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
-
+    const [error, setError] = useState(false)
     // optionType is 'scoops' or 'toppings'
     useEffect(() => {
         axios
             .get(`http://localhost:3030/${optionType}`)
             .then((response) => setItems(response.data))
-            .catch((error) => console.log(error));
+            .catch((error) => setError(true));
     }, [optionType]);
+
+    if (error) {
+
+        return <AlertBanner />;
+    }
 
 
     const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
---
+++ src/pages/entry/OrderEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Options from './Options'
+const OrderEntry = () => {
+    return (
+        <div>
+            <Options optionType='scoops' />
+            <Options optionType='toppings' />
+        </div>
+    )
+}
+
+export default OrderEntry
